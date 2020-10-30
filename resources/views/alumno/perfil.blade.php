1afd1a811c774a8c3e0af2e9831748ee69019dfb
SheylaAlejosCuya <zurang.sheyla@gmail.com>
2020-10-30 16:37:43 -0500

    Primer commit

---
+++ resources/views/alumno/perfil.blade.php
@@ -0,0 +1,71 @@
+@include('includes.menubar')
+<!-- Content Wrapper. Contains page content -->
+<style>
+ 
+</style>
+<div class="content-wrapper" id="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="infomacion">
+    <h1><strong>Información Básica</strong></h1>
+       <div class="row">
+           <div class="col">
+                <div class="row">
+                    <div class="col">
+                        <h2><strong>Nombre Completo</strong></h2>
+                    </div>
+                    <div class="col">
+                        Cathy Vega
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col">
+                        <h2><strong>E-mail</strong></h2>
+                    </div>
+                    <div class="col">
+                        Cathy@micolegio.com
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col">
+                        <h2><strong>Cumpleaños</strong></h2>
+                    </div>
+                    <div class="col">
+                        20/03/2010
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col">
+                        <h2><strong>Usuario</strong></h2>
+                    </div>
+                    <div class="col">
+                        12345
+                    </div>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col">
+                        <h2><strong>Cambiar Contraseña</strong></h2>
+                    </div>
+                    <div class="col">
+                        Cathy Vega
+                    </div>
+                </div>
+           </div>
+           <div class="col-lg">
+                <h2><strong>Avatar</strong></h2>
+                <img src="images/chica.png" alt="" width="20%">
+                <img src="images/chica.png" alt="" width="20%">
+                <img src="images/chica.png" alt="" width="20%">
+           </div>
+       </div>
+    </div>
+    <div id="botones">
+            <button id="guia">Guardar</button>
+            <button id="tuto">Cancelar</button>
+        </div>
+</div>
+
+@include('includes.footer')
