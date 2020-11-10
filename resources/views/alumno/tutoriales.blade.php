d02c1b68f4f9778f877cbf5e1c0d8389945a2942
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-11-10 12:19:19 -0500

    tutorialVideo to div

--- resources/views/alumno/tutoriales.blade.php
+++ resources/views/alumno/tutoriales.blade.php
@@ -1,19 +1,19 @@
 <div class="infomacion">
         <h1><strong>Tutoriales</strong></h1>
         <div class="tutorialVideos">
-            <div href="tutoriales/1" class="tutorialVideo" onClick="selectTutorial()">
+            <div class="tutorialVideo" onClick="selectTutorial()">
                 <i class="far fa-play-circle"></i>
                 <div>Demo 1</div>
             </div>
-            <div href="tutoriales/2" class="tutorialVideo" onClick="selectTutorial()">
+            <div class="tutorialVideo" onClick="selectTutorial()">
                 <i class="far fa-play-circle"></i>
                 <div>Demo 2</div>
             </div>
-            <div href="tutoriales/3" class="tutorialVideo" onClick="selectTutorial()">
+            <div class="tutorialVideo" onClick="selectTutorial()">
                 <i class="far fa-play-circle"></i>
                 <div>Demo 3</div>
             </div>
-            <div href="tutoriales/4" class="tutorialVideo" onClick="selectTutorial()">
+            <div class="tutorialVideo" onClick="selectTutorial()">
                 <i class="far fa-play-circle"></i>
                 <div>Demo 4</div>
             </div>
