a9e7a285670135abaaac8725c24333606b7c8e9a
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-11-10 12:17:58 -0500

    tutoriales arreglado

--- resources/views/alumno/tutoriales.blade.php
+++ resources/views/alumno/tutoriales.blade.php
@@ -1,22 +1,22 @@
 <div class="infomacion">
         <h1><strong>Tutoriales</strong></h1>
         <div class="tutorialVideos">
-            <a href="tutoriales/1" class="tutorialVideo" onClick="selectTutorial()">
+            <div href="tutoriales/1" class="tutorialVideo" onClick="selectTutorial()">
                 <i class="far fa-play-circle"></i>
                 <div>Demo 1</div>
-            </a>
-            <a href="tutoriales/2" class="tutorialVideo" onClick="selectTutorial()">
+            </div>
+            <div href="tutoriales/2" class="tutorialVideo" onClick="selectTutorial()">
                 <i class="far fa-play-circle"></i>
                 <div>Demo 2</div>
-            </a>
-            <a href="tutoriales/3" class="tutorialVideo" onClick="selectTutorial()">
+            </div>
+            <div href="tutoriales/3" class="tutorialVideo" onClick="selectTutorial()">
                 <i class="far fa-play-circle"></i>
                 <div>Demo 3</div>
-            </a>
-            <a href="tutoriales/4" class="tutorialVideo" onClick="selectTutorial()">
+            </div>
+            <div href="tutoriales/4" class="tutorialVideo" onClick="selectTutorial()">
                 <i class="far fa-play-circle"></i>
                 <div>Demo 4</div>
-            </a>
+            </div>
         </div>
 </div>
 <script>
