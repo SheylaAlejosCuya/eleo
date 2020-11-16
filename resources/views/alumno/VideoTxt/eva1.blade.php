94c4a8a3b11ca6185c36b2556664b0f52986a91c
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2020-11-16 13:12:16 -0500

    videoPreguntas mejorado

--- resources/views/alumno/VideoTxt/eva1.blade.php
+++ resources/views/alumno/VideoTxt/eva1.blade.php
@@ -1,36 +1,31 @@
 <link rel="stylesheet" href="{{asset('css/lecturas.css')}}">
 <link rel="stylesheet" href="{{asset('css/videos.css')}}">
 <div class="infomacion">
-        <div>
-            <div id="p1">
-                <p><b>Luego de ver el video introductorio, responde a las siguientes preguntas</b></p>          
+    
+    <div class="epreguntas-c">
+        <div id="p1">
+            <p><b>Luego de ver el video introductorio, responde a las siguientes preguntas</b></p>
+            <div class="iconContainer">
+                <img class="actionButton" src="{{asset('images/escucho.png')}}" alt="">
+                <img class="actionButton" src="{{asset('images/escribe.png')}}" alt="">
             </div>
-            <hr>
-            <div class="row answerContent">
-                <div class="col-lg rsp">
-                    <p>
-                    ¿Qué pensaban los egipcios sobre la muerte?
-                    </p>
-                    <textarea class="rspInput" rows="4" cols="50" placeholder="Escribe tu respuesta"></textarea>
-                </div>
-                <div class="col">
-
-                </div>
-            </div>
-            <div class="row answerContent">
-                <div class="col-lg rsp">
-                    <p>
-                    ¿Para qué se hacían las momificaciones?
-                    </p>
-                    <textarea class="rspInput" rows="4" cols="50" placeholder="Escribe tu respuesta"></textarea>
-                </div>
-                <div class="col">
-
-                </div>
-            </div>
-            
         </div>
-        <div class="ebuttons" style="font-family:'Nunito', sans-serif;"> 
+        <hr>
+        <div class="epreguntas">
+            <div class="rpt">
+                <h5>¿Qué pensaban los egipcios sobre la muerte?</h5>
+                <textarea class="rspInput" rows="4" cols="50" placeholder="Escribe tu respuesta"></textarea>
+            </div>
+        </div>
+        <br>
+        <div class="epreguntas">
+            <div class="rpt">
+                <h5>¿Para qué se hacían las momificaciones?</h5>
+                <textarea class="rspInput" rows="4" cols="50" placeholder="Escribe tu respuesta"></textarea>
+            </div>
+        </div>
+    </div>
+    <div class="ebuttons" style="font-family:'Nunito', sans-serif;"> 
         <button class="saveButton">Verifica</button>
         <button class="cancelButton">Avanza</button>
     </div>
