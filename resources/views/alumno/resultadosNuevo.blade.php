a1267f155ead7a491f3d0c993fff51a0dddf32c5
LeonardoCuraca <leonardo.curaca@tecsup.edu.pe>
2021-01-21 22:10:21 -0500

    promedio general Alumno

---
+++ resources/views/alumno/resultadosNuevo.blade.php
@@ -0,0 +1,37 @@
+<link rel="stylesheet" href="{{asset('css/libros.css')}}">
+<div class="infomacion">
+    <h4>{{$subtitle}}</h4>
+    <div class="ui two column centered stackable grid">
+        <div class="row">
+            <div class="column">
+                <div class="ui centered grid">
+                    <div class="eleven wide column">
+                        <a href="./resultados/estudio" class="ui fluid image">
+                            <img class="desafioImg" src="images/resultado1.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="ui doubling centered grid">
+                    <div class="eleven wide column">
+                        <a href="./resultados/estudio" class="ui fluid image">
+                            <img class="desafioImg" src="images/resultado2.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column">
+                <div class="ui centered grid">
+                    <div class="eleven wide column">
+                        <a href="./resultadosNuevo/promedioGeneral" class="ui fluid image">
+                            <img class="desafioImg" src="images/promedio.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/alumno/resultadosPromedio.blade.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" href="{{asset('css/resultados.css')}}">
+<div class="infomacion">
+    <br>
+    <div class="ui stackable grid">
+        <div class="ten wide column">
+            <x-result-progress-bar title="COMPRENSIÓN AUDITIVA" :results="$aresults" />
+            <x-result-progress-bar title="COMPRENSIÓN LECTORA" :results="$lresults" />
+            <x-result-progress-bar title="COMPRENSIÓN DE TEXTOS" :results="$tresults" />
+        </div>
+        <div class="six wide column">
+            <div class="eresultadosCards">
+                <div class="eresultadosCard">
+                    <div class="eresultadosCard__left">
+                        <img src="{{asset('images/gorro.png')}}" alt="">
+                        100%
+                    </div>
+                    <div class="eresultadosCard__right">
+                        <div class="ecardText">
+                            <p><b>Comprensión Auditiva</b></p>
+                            <p>¡Haz completado 10 lecturas!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="eresultadosCard">
+                    <div class="eresultadosCard__left">
+                        <img src="{{asset('images/gorro.png')}}" alt="">
+                        100%
+                    </div>
+                    <div class="eresultadosCard__right">
+                        <div class="ecardText">
+                            <p><b>Comprensión Lectora</b></p>
+                            <p>¡Haz completado 10 lecturas!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="eresultadosCard">
+                    <div class="eresultadosCard__left">
+                        <img src="{{asset('images/gorro.png')}}" alt="">
+                        100%
+                    </div>
+                    <div class="eresultadosCard__right">
+                        <div class="ecardText">
+                            <p><b>Comprensión de Textos</b></p>
+                            <p>¡Haz completado 10 lecturas!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
