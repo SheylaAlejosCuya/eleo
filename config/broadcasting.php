2b6107a3eb37e4f2fd0770246a06bf0983f96924
RonaldMiya <ronald.miya@tecsup.edu.pe>
2021-05-07 04:18:10 -0500

    chat [chat]

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,10 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => false,
+                'useTLS' => true,
                 'host' => 'e-leo.com.pe',
                 'port' => 6001,
-                'scheme' => 'http',
+                'scheme' => 'https',
             ],
         ],
 
