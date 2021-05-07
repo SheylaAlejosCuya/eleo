488b507e44ae25246ad9013722952d44f32e6d03
RonaldMiya <ronald.miya@tecsup.edu.pe>
2021-05-07 03:07:43 -0500

    chat [chat]

--- public/js/app.js
+++ public/js/app.js
@@ -2549,7 +2549,7 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__.default({
   // cluster: process.env.MIX_PUSHER_APP_CLUSTER,
   forceTLS: false,
   wsHost: window.location.hostname,
-  wsPort: 5000,
+  wsPort: 6001,
   disableStats: true
 });
 
